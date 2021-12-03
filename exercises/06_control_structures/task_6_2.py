994892a9b6f876b37590afbc8fcf209e5dcfb2ef
levtes <levtes5004@gmail.com>
2021-12-03 02:30:08 -0600

    added 2.js

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,14 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+a = input().split(".")
+if 1 <= int(a[0]) <= 223:
+    print("unicast")
+elif 224 <= int(a[0]) <= 239:
+    print("multicast")
+elif a[0] == "255" and a[1] == "255" and a[2] == "255" and a[3] == "255":
+    print("local broadcast")
+elif a[0] == "0" and a[1] == "0" and a[2] == "0" and a[3] == "0":
+    print("unassigned")
+else:
+    print("unused")
