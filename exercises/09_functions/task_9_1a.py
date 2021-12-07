9c056d700c4018e6b727e3dbbf1fe49f8f30bdd4
levtes <levtes5004@gmail.com>
2021-12-07 04:51:57 -0600

    Ещё одно задание сделал

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -43,3 +43,12 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+def generate_access_config(intf_vlan_mapping, access_template, *port_security):
+    for i in intf_vlan_mapping.keys():
+        restr = access_template[1]
+        access_template[1] += " " + str(intf_vlan_mapping[i])
+        print(i, end = "\n")
+        print(*access_template, sep = "\n")
+        access_template[1] = restr
+        print(*port_security_template,sep = "\n")
+generate_access_config(access_config, access_mode_template)
